bd68133428154725fb6fd6c28f0c7b7ad42131f7
Your Namecls <junejowaseem0086@gmail.com>
2025-01-04 21:22:18 +0500

    first commit

---
+++ src/app/Blogs/page.tsx
@@ -0,0 +1,14 @@
+import Sec1 from "@/components/blogSec"
+import Header from "@/components/Navbar"
+
+function homePage() {
+  return (
+    <>
+    <Header />
+    <Sec1 />
+    
+    </>
+  )
+}
+
+export default homePage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
+
 function Homepage() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <>
+    <div className="max-w-[1450px] mx-auto bg-orange-500">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-5xl font-bold text-center">Welcome to {"Waseem's"} Blog</h1>
+        <p className="text-lg text-gray-600 my-5">Discover the latest blog posts, tutorials, and articles.</p>
+        <Link href="/Blogs"><button className="py-3 px-8 text-white font-bold bg-gray-800 rounded-md hover:bg-gray-700">Explore Blogs</button></Link>
+      </div>
     </div>
+    
+    </>
   )
 }
 
-export default Homepage
+export default Homepage;
+
+
+
